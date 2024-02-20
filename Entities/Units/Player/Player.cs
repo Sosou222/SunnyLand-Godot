0bc30dd80178b9172612d0219594a2b55ad44cc0
Sosou222 <szym.baran222@gmail.com>
2024-02-20 09:45:42 +0100

    added basic collectible behaviour

--- Entities/Units/Player/Player.cs
+++ Entities/Units/Player/Player.cs
@@ -3,5 +3,27 @@ using System;
 
 public partial class Player : Unit
 {
+    private int cherryCount = 0;
+    private int gemCount = 0;
 
+    [Signal]
+    public delegate void CherryCountChangeEventHandler(int newValue);
+    [Signal]
+    public delegate void GemCountChangeEventHandler(int newValue);
+
+    public void AddCollectible(string name, int amount = 1)
+    {
+        if (name == "Cherry")
+        {
+            cherryCount += amount;
+            EmitSignal(SignalName.CherryCountChange, cherryCount);
+            GD.Print($"Cherry count:{cherryCount}");
+        }
+        if (name == "Gem")
+        {
+            gemCount += amount;
+            EmitSignal(SignalName.GemCountChange, gemCount);
+            GD.Print($"Gem count:{gemCount}");
+        }
+    }
 }
--- Scripts/Components/Collectible.cs
+++ Scripts/Components/Collectible.cs
@@ -3,11 +3,17 @@ using System;
 
 public partial class Collectible : Area2D
 {
+	[Export]
+	private string collectibleName = "";
+
 	private void OnBodyEnter(Node2D node)
 	{
 		if (node is Player player)
 		{
 			GD.Print($"Body entered:{player.Name}");
+			player.AddCollectible(collectibleName);
+
+			SetDeferred("monitoring", false); // Can't use 'Monitoring = false;' beacuse it is 'monitoring' currently. Probably would work outside this signal function...mabye
 		}
 	}
 }
