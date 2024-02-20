198820277e2f89f7cb07832a074188bc2838c8c7
Sosou222 <szym.baran222@gmail.com>
2024-02-20 10:18:56 +0100

    added animation tweens to collectible

--- Scripts/Components/Collectible.cs
+++ Scripts/Components/Collectible.cs
@@ -6,6 +6,15 @@ public partial class Collectible : Area2D
 	[Export]
 	private string collectibleName = "";
 
+	private string pickupAniamtion = "PickUp";
+
+	private AnimationPlayer animationPlayer;
+
+	public override void _Ready()
+	{
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
 	private void OnBodyEnter(Node2D node)
 	{
 		if (node is Player player)
@@ -14,6 +23,25 @@ public partial class Collectible : Area2D
 			player.AddCollectible(collectibleName);
 
 			SetDeferred("monitoring", false); // Can't use 'Monitoring = false;' beacuse it is 'monitoring' currently. Probably would work outside this signal function...mabye
+
+			PickupTween();
 		}
+	}
+
+	private void PickupTween()
+	{
+		var tween = GetTree().CreateTween();
+		tween.TweenProperty(this, "position", Position - new Vector2(0, 50), 0.5);
+
+		tween.TweenCallback(new Callable(this, MethodName.PlayPickupAnimation));
+	}
+
+	private void PlayPickupAnimation()
+	{
+		animationPlayer.Play(pickupAniamtion);
+		var tween = animationPlayer.CreateTween();
+		tween.TweenProperty(this, "modulate:a", 0, 0.4f);
+
+		tween.TweenCallback(new Callable(this, MethodName.QueueFree));
 	}
 }
