2ff7ba0c459582a3ac8cda693aa3a61d75e705bf
Sosou222 <szym.baran222@gmail.com>
2024-02-21 14:23:47 +0100

    added some simple enemy logic

--- Entities/Units/Enemies/Enemy.cs
+++ Entities/Units/Enemies/Enemy.cs
@@ -3,5 +3,41 @@ using System;
 
 public partial class Enemy : Unit
 {
+    [Export]
+    private RayCast2D toPlayerRaycast;
 
+    private Player player = null;
+    private void OnBodyEntered(Node2D node)
+    {
+        if (node is Player p)
+        {
+            GD.Print("Player entered");
+            player = p;
+        }
+    }
+
+    private void OnBodyExit(Node2D node)
+    {
+        if (node is Player p)
+        {
+            GD.Print("Player Exited");
+            player = null;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        if (player == null)
+            return;
+
+        GD.Print(IsPlayerInSight());
+    }
+
+    public bool IsPlayerInSight()
+    {
+        toPlayerRaycast.TargetPosition = player.GlobalPosition;
+        return !toPlayerRaycast.IsColliding();
+    }
 }
